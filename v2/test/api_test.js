968fdac139a7382736e5005458d3a3a5dbc14f54
Joel Kraft <joelkraft3@yahoo.com>
2016-11-26 12:29:26 -0800

    add test for GET /:id route, rework tests to be more modular

--- v2/test/api_test.js
+++ v2/test/api_test.js
@@ -4,36 +4,62 @@ const app = require('../app');
 // const manifestList = require('../flat/manifest-list');
 const Manifest = require('../models').Manifest;
 
-after(done => {
+describe('Routes to retrieve manifests', () => {
+  before(done => {
+    Promise.all([5,6,7,8].map(num => new Manifest(require(`../flat/esp8266/esp12/espruino/manifest.1.8${num}.json`)).save()))
+    .then(() => new Manifest(require('../flat/esp8266/esp12/smartjs/manifest.json')).save())
+    .then(() => {done()})
+  });
+
+  after(done => {
   Manifest.db.dropDatabase()
   .then(() => done()) 
-})
+  });
 
-describe('GET root route', () => {
-  it('should return a manifest-list', done => {
-  Promise.all([5,6,7,8].map(num => new Manifest(require(`../flat/esp8266/esp12/espruino/manifest.1.8${num}.json`)).save()))
-  .then(() => new Manifest(require('../flat/esp8266/esp12/smartjs/manifest.json')).save())
-  .then(() => {
-    request(app)
-      .get('/v2')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end((err, res) => {
-        expect(err).to.be.null;
+  describe('GET root route', () => {
+    it('should return a manifest-list', done => {
+      request(app)
+        .get('/v2')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end((err, res) => {
+          expect(err).to.be.null;
 
-        const manifestList = res.body;
-        expect(manifestList).to.have.property('options').that.is.an('array');
+          const manifestList = res.body;
+          expect(manifestList).to.have.property('options').that.is.an('array');
 
-        const firmware = manifestList.options[0];
-        expect(firmware).to.have.property('name').that.is.a('string');
-        expect(firmware).to.have.property('versions').that.is.an('array');
+          const firmware = manifestList.options[0];
+          expect(firmware).to.have.property('name').that.is.a('string');
+          expect(firmware).to.have.property('versions').that.is.an('array');
 
-        const version = firmware.versions[0];
-        expect(version).to.have.all.keys(['version', 'board', 'revision', 'manifest', 'latest']);
-        done();
-      });
+          const version = firmware.versions[0];
+          expect(version).to.have.all.keys(['version', 'board', 'revision', 'manifest', 'latest']);
+          done();
+        });
     });
   });
+
+  describe('GET /:id', () => {
+    it('should return a manifest', done => {
+      Manifest.findOne({name: 'Espruino', board: 'ESP8266', version: '1v85'}).exec()
+      .then(doc => {
+        request(app)
+          .get(`/v2/${doc._id}`)
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end((err, res) => {
+            expect(res.body).to.have.property('name', 'Espruino');
+            expect(res.body).to.have.property('version', '1v85');
+            expect(res.body).to.have.property('board', 'ESP8266');
+            expect(res.body).to.have.property('revision', 'ESP-12');
+            expect(res.body).to.have.property('description', 'Official Binaries for the Espruino Runtime for the ESP8266 MCU ESP-12');
+            expect(res.body).to.have.property('download', 'http://www.espruino.com/files/espruino_1v85.zip');
+            expect(res.body).to.have.property('flash').that.is.an('array');
+            done();
+          })
+      }).catch(err => {throw err});
+    })
+  })
 });
 
 // describe('GET /:microcontroller/:firmware/:version', () => {
--- v2/test/model_test.js
+++ v2/test/model_test.js
@@ -5,43 +5,43 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const Manifest = require('../models').Manifest;
 
-const removeAll = (done) => Manifest.remove({}).then(()=>done());
+describe('Models', () => {
+  describe('Manifest model', () => {
+    beforeEach((done) => {Manifest.remove({}).then(()=>done())});
+    after(done => {Manifest.db.dropDatabase().then(() => done())});
 
-beforeEach(done => { removeAll(done) });
-afterEach(done => { removeAll(done) });
-
-describe('Manifest model', () => {
-  it('must save a manifest', done => {
-    const manifest = new Manifest(testManifest);
-    manifest.save((err, manifest) => {
-      expect(err).to.be.null;
-      expect(manifest).to.have.property('name', 'Espruino');
-      expect(manifest).to.have.property('version', '1v85');
-      expect(manifest).to.have.property('board', 'ESP8266');
-      expect(manifest).to.have.property('revision', 'ESP-12');
-      expect(manifest).to.have.property('description', 'Official Binaries for the Espruino Runtime for the ESP8266 MCU ESP-12');
-      expect(manifest).to.have.property('download', 'http://www.espruino.com/files/espruino_1v85.zip');
-      expect(manifest).to.have.property('flash').that.is.an('array');
-      done();
-    });
-  });
-
-  it('must retrieve a manifest', done => {
-    const manifest = new Manifest(testManifest);
-    manifest.save()
-      .then(manifest => Manifest.find({board: 'ESP8266', revision: 'ESP-12'}).exec())
-      .then(manifests => {
-        expect(manifests.length).to.eql(1);
-        const manifest = manifests[0];
+    it('must save a manifest', done => {
+      const manifest = new Manifest(testManifest);
+      manifest.save((err, manifest) => {
+        expect(err).to.be.null;
         expect(manifest).to.have.property('name', 'Espruino');
         expect(manifest).to.have.property('version', '1v85');
         expect(manifest).to.have.property('board', 'ESP8266');
         expect(manifest).to.have.property('revision', 'ESP-12');
         expect(manifest).to.have.property('description', 'Official Binaries for the Espruino Runtime for the ESP8266 MCU ESP-12');
         expect(manifest).to.have.property('download', 'http://www.espruino.com/files/espruino_1v85.zip');
-        expect(manifest).to.have.property('flash').that.is.an('array');        
+        expect(manifest).to.have.property('flash').that.is.an('array');
         done();
-      })
-      .catch(err => {throw err})
+      });
+    });
+
+    it('must retrieve a manifest', done => {
+      const manifest = new Manifest(testManifest);
+      manifest.save()
+        .then(manifest => Manifest.find({board: 'ESP8266', revision: 'ESP-12'}).exec())
+        .then(manifests => {
+          expect(manifests.length).to.eql(1);
+          const manifest = manifests[0];
+          expect(manifest).to.have.property('name', 'Espruino');
+          expect(manifest).to.have.property('version', '1v85');
+          expect(manifest).to.have.property('board', 'ESP8266');
+          expect(manifest).to.have.property('revision', 'ESP-12');
+          expect(manifest).to.have.property('description', 'Official Binaries for the Espruino Runtime for the ESP8266 MCU ESP-12');
+          expect(manifest).to.have.property('download', 'http://www.espruino.com/files/espruino_1v85.zip');
+          expect(manifest).to.have.property('flash').that.is.an('array');        
+          done();
+        })
+        .catch(err => {throw err})
+    })
   })
-})
+});
